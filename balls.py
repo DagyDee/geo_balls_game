e3da627d98e1b42fb38fd3b7785b03138775b46d
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-06-03 22:56:50 +0200

    code review
    update images, rename variables

--- balls.py
+++ balls.py
@@ -6,25 +6,25 @@ WIDTH = 1920
 HEIGHT = 1080
 
 # images
-red_ball = pyglet.image.load("red.png")
-yellow_ball = pyglet.image.load("yellow.png")
+IMAGE_SIZE = 64
+IMAGE_FRONT_SIDE = pyglet.image.load("green_ball.png")
+IMAGE_BACK_SIDE = pyglet.image.load("red_ball.png")
 
-labels = ["A=5", "B=3", "C=7", "D=1", "E=9", "F=2"]
+LABELS = ["A=5", "B=3", "C=7", "D=1", "E=9", "F=2"]
 
 class Ball:
     def __init__(self, hide_text):
         self.hide_text = hide_text
 
     def initialize(self):
-        self.size = 64
         self.speed = 200
-        self.position = [random.randint(0, WIDTH), random.randint(0, HEIGHT)]  # coordinates x, y
-        self.direction = [random.uniform(-1, 1), random.uniform(-1, 1)]  #  in axes x, y
-        self.ball_image = red_ball
+        self.ball_image = IMAGE_FRONT_SIDE
         self.show_text = False
+        self.position = [random.randint(0, WIDTH), random.randint(0, HEIGHT)]  # coordinates x, y
+        self.direction = [random.uniform(-1, 1), random.uniform(-1, 1)]  # in axes x, y
 
     def draw(self):
-        self.picture = pyglet.sprite.Sprite(self.ball_image, self.position[0] - self.size // 2, self.position[1] - self.size // 2)
+        self.picture = pyglet.sprite.Sprite(self.ball_image, self.position[0] - IMAGE_SIZE // 2, self.position[1] - IMAGE_SIZE // 2)
         self.picture.draw()
         self.label = pyglet.text.Label(self.hide_text, font_size=20, color=(0, 0, 0), x=self.position[0], y=self.position[1], anchor_x='center', anchor_y='center')
         if self.show_text == True:
@@ -35,32 +35,32 @@ class Ball:
         self.position[1] += self.direction[1] * dt * self.speed
 
         # movement barriers
-        if self.position[0] < self.size // 2:
+        if self.position[0] < IMAGE_SIZE // 2:
             self.direction[0] = abs(self.direction[0])
-        if self.position[0] > WIDTH - self.size // 2:
+        if self.position[0] > WIDTH - IMAGE_SIZE // 2:
             self.direction[0] = -abs(self.direction[0])
-        if self.position[1] < self.size // 2:
+        if self.position[1] < IMAGE_SIZE // 2:
             self.direction[1] = abs(self.direction[1])
-        if self.position[1] > HEIGHT - self.size // 2:
+        if self.position[1] > HEIGHT - IMAGE_SIZE // 2:
             self.direction[1] = -abs(self.direction[1])
     
     def show_label(self, x, y, button, modifiers):
-        if x >= self.position[0] - self.size // 2 and x <= self.position[0] + self.size // 2:
-            if y >= self.position[1] - self.size // 2 and y <= self.position[1] + self.size // 2:
+        if x >= self.position[0] - IMAGE_SIZE // 2 and x <= self.position[0] + IMAGE_SIZE // 2:
+            if y >= self.position[1] - IMAGE_SIZE // 2 and y <= self.position[1] + IMAGE_SIZE // 2:
                 self.show_text = True
-                self.ball_image = yellow_ball
+                self.ball_image = IMAGE_BACK_SIDE
 
                 pyglet.clock.schedule_once(self.hide_label, 0.5)
                 
     def hide_label(self, t):
         self.show_text = False
-        self.ball_image = red_ball
+        self.ball_image = IMAGE_FRONT_SIDE
 
 # creating objects:
 balls = []
 
 # create balls with secret
-for label in labels:
+for label in LABELS:
     ball = Ball(label)
     ball.initialize()
     balls.append(ball)
---
+++ read_me.txt
@@ -0,0 +1,1 @@
+Click on a ball and will seen the secret!
